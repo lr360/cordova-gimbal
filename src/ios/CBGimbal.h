4699433c8d3c42587dfc85cdba122b870c66bea0
Sebastien Guimont <sebastieng@sympatico.ca>
2015-02-24 18:54:39 -0500

    adjust method

--- src/ios/CBGimbal.h
+++ src/ios/CBGimbal.h
@@ -1,8 +1,14 @@
+#ifndef Beacon_gimbal_h
+#define Beacon_gimbal_h
+
 #import <Cordova/CDV.h>
+#import <Cordova/CDVPlugin.h>
 
 @interface CBGimbal : CDVPlugin
 
-- (void) deviceready:(CDVInvokedUrlCommand*)command;
+- (void)deviceready:(CDVInvokedUrlCommand*)command;
+- (void)startService:(CDVInvokedUrlCommand*)command;
 
 @end
 
+#endif
